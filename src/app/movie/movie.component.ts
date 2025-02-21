bf8cf17a5b2be475209322d27f920193aae4dced
Bogdan Kostadinov <bogdan.kostadinov1@gmail.com>
2025-02-21 00:40:36 +0000

    introducing chip categories

--- src/app/movie/movie.component.ts
+++ src/app/movie/movie.component.ts
@@ -1,6 +1,12 @@
 import { Component, OnInit } from '@angular/core';
 import { Movie } from '../shared/models/movie.model';
 import { MovieService } from '../shared/services/movie.service';
+import { MatChipSelectionChange } from '@angular/material/chips';
+
+export interface Category {
+  name: string;
+  selected: boolean;
+}
 
 @Component({
   selector: 'app-movie',
@@ -10,6 +16,13 @@ import { MovieService } from '../shared/services/movie.service';
 export class MovieComponent implements OnInit {
   movies: Movie[] = [];
   filteredMovies: Movie[] = [];
+  movieCategories: Category[] = [
+    { name: 'Action', selected: false },
+    { name: 'Adventure', selected: false },
+    { name: 'Comedy', selected: false },
+    { name: 'Drama', selected: false },
+    { name: 'Horror', selected: false },
+  ];
   searchValue = '';
   constructor(private movieService: MovieService) {}
 
@@ -19,7 +32,26 @@ export class MovieComponent implements OnInit {
       .subscribe((movies) => (this.filteredMovies = this.movies = movies));
   }
 
-  filterMovies() {
+  selectChip(event: MatChipSelectionChange, category: Category): void {
+    category.selected = event.selected;
+    this.movieCategories = this.movieCategories.map((cat) =>
+      cat.name === category.name ? category : cat,
+    );
+    this.filterMoviesByCategory();
+  }
+
+  filterMoviesByCategory(): void {
+    this.filteredMovies =
+      this.movieCategories.filter((cat) => cat.selected).length > 0
+        ? this.movies.filter((movie) =>
+            this.movieCategories
+              .filter((cat) => cat.selected)
+              .some((cat) => movie.genre.includes(cat.name)),
+          )
+        : this.movies;
+  }
+
+  searchMovies() {
     this.filteredMovies = this.searchValue
       ? this.movies.filter((movie) =>
           movie.title.toLowerCase().includes(this.searchValue.toLowerCase()),
