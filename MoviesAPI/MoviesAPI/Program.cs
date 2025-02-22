72cd5d25df1ad76f6e78afb3750f71bd68c9999c
Bogdan Kostadinov <bogdan.kostadinov1@gmail.com>
2025-02-22 01:13:53 +0000

    adding initial migration

--- MoviesAPI/MoviesAPI/Program.cs
+++ MoviesAPI/MoviesAPI/Program.cs
@@ -1,6 +1,8 @@
+using Microsoft.EntityFrameworkCore;
+using MoviesAPI.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
 builder.Services.AddCors(options =>
 {
   options.AddPolicy("AllowAngularApp",
@@ -11,10 +13,15 @@ builder.Services.AddCors(options =>
                 .AllowAnyHeader();
       });
 });
+
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddDbContext<DataContext>(options =>
+{
+  options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
+});
 
 var app = builder.Build();
 app.UseCors("AllowAngularApp");
